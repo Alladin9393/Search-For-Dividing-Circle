ca7a22d4c5dcc56a438fd2a61acf192c73959d05
Vlad Pavlichek <ember.toon@protonmail.com>
2020-02-24 14:45:19 +0200

    Implement DividingCircle

---
+++ SearchForDividingCircle/DividingCircle/interfaces.py
@@ -0,0 +1,25 @@
+"""Provide implementation of the Search-For-Dividing-Circle by the perceptron interface."""
+
+
+class DividingCircleInterface:
+    """Implements Search-For-Dividing-Circle by the perceptron interface."""
+
+    def generate_points(self):
+        """Generate start data for algorithm."""
+        pass
+
+    def _random_divide_points(self, points):
+        """Random point assignment inside or outside the circle."""
+        pass
+
+    def _get_center_coordinates(self):
+        """Get center coordinates of the Circle."""
+        pass
+
+    def _get_radius(self):
+        """Get radius of the Circle."""
+        pass
+
+    def _get_area_coordinates(self):
+        """Random point assignment inside or outside the circle."""
+        pass
---
+++ SearchForDividingCircle/main.py
@@ -0,0 +1,47 @@
+"""Provide Search-For-Dividing-Circle by the perceptron."""
+
+import numpy as np
+
+from SearchForDividingCircle.DividingCircle.DividingCircle import DividingCircle
+
+if __name__ == '__main__':
+    center_circle_start_coordinates = {
+        'x': -10,
+        'y': -10,
+    }
+    center_circle_end_coordinates = {
+        'x': 10,
+        'y': 10,
+    }
+
+    DC = DividingCircle(
+        radius_range=(1, 7),
+        center_circle_start_coordinates=center_circle_start_coordinates,
+        center_circle_end_coordinates=center_circle_end_coordinates,
+        amount_points=1000,
+    )
+
+    DC.generate_points()
+    print(DC.points.shape)
+    print(DC.inner_points.shape)
+    print(DC.outer_points.shape)
+    print(DC.x_train.shape)
+    print(DC.y_train.shape)
+    perceptron_params = DC.train()
+
+    print("center_x_coordinate : {} , center_y_coordinates : {}, radius : {}".format(
+        DC.center_coordinates.get('x'),
+        DC.center_coordinates.get('y'),
+        DC.radius,
+    ))
+
+    center_x_coordinate_prediction = (-1 / 2) * (perceptron_params[2] / perceptron_params[1])
+    center_y_coordinate_prediction = (-1 / 2) * (perceptron_params[3] / perceptron_params[1])
+    radius_prediction = np.sqrt(center_x_coordinate_prediction ** 2 + center_y_coordinate_prediction ** 2
+                                - perceptron_params[0] / perceptron_params[1])
+
+    print("\ncenter_x_coordinate_prediction : {} , center_y_coordinate_prediction : {}, radius_prediction : {}".format(
+        center_x_coordinate_prediction,
+        center_y_coordinate_prediction,
+        radius_prediction,
+    ))
